5ea2406a0e4141e9089fa255d5f8eaf14753b5ed
pascal <okeziepascal2@gmail.com>
2024-02-08 19:05:36 +0100

    W04: Programming Task updated

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -8,48 +8,55 @@ let myProfile = {
     favoriteFoods:["Rice and Chicken Stew", "Suya", "Fried Rice", "Chicken", "Beef", "Yam and egg sauce"],
     hobbies: ["Cooking", "Reading", "Travelling"],
     placesLived: [],
-
 };
     
 
 /* Populate Profile Object with placesLive objects */
 
-myProfile["placesLived"] = {
-    place: "Lagos, Nigeria",
-    length: "1 year"
-}
-
+myProfile.placesLived.push(
+    {place: "lagos", length: "5 years"},
+    {place: "Abeokuta", length: "3 years"},
+    {place: "Sango", length: "1 year"}
+);
 
 /* DOM Manipulation - Output */
 
 /* Name */
-document.querySelector("#name").textContent = myProfile["name"];
+document.getElementById("name").innerText = myProfile.name;
+
 /* Photo with attributes */
-document.querySelector("img").setAttribute("src", myProfile["photo"]);
-doocument.querySelector("img").setAttribute("alt", "Profile Image");
+
+document.getElementById("photo").src = myProfile.photo;
+document.getElementById("photo").alt = myProfile.name;
+
 /* Favorite Foods List*/
-myProfile.favoriteFoods.forEach(food => {
-    let li = document.createElement('li');
-    li.textContent = food;
-    document.querySelector('#favorite-foods').appendChild(li);
-    });
+myProfile.favoriteFoods.forEach(function(food) {
+    let listItem = document.createElement("li");
+    listItem.textContent = food;
+    document.getElementById("favorite-foods").appendChild(listItem);
+});
+
 /* Hobbies List */
-myProfile.hobbies.forEach(hobby => {
-    let li = document.createElement('li');
-    li.textContent = hobby;
-    document.querySelector('#hobbies').appendChild(li);
-    });
+myProfile.hobbies.forEach(function(hobby) {
+    let listItem = document.createElement("li");
+    listItem.textContent = hobby;
+    document.getElementById("hobbies").appendChild(listItem);
+});
+
+
 /* Places Lived DataList */
-const placesLived = myProfile["placesLived"];
-const dl = document.querySelector('#places-lived');
-
-for (const key in placesLived) {
-  const dt = document.createElement('dt');
-  dt.textContent = key;
-  dl.appendChild(dt);
-
-  const dd = document.createElement('dd');
-  dd.textContent = placesLived[key];
-  dl.appendChild(dd);
-}
+myProfile.placesLived.forEach(function(placeInfo) {
+    
+    let dtElement = document.createElement("dt");
+    
+    dtElement.textContent = placeInfo.place;
+    
+    
+    let ddElement = document.createElement("dd");
+    
+    ddElement.textContent = placeInfo.length;
+    
+    document.getElementById("places-lived").appendChild(dtElement);
+    document.getElementById("places-lived").appendChild(ddElement);
+});
 
